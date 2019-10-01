d5f41d0da1e821da408ab648500ce30cc9c969d1
maurice_jm <maurice@mokaza.com>
2019-10-01 15:53:48 +0400

    a delg

--- Swif-CoreData_3.1/AppDelegate.swift
+++ Swif-CoreData_3.1/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        // after the app launches
 
         return true
     }
